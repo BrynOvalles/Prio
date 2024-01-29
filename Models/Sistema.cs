4afceff1dd1d92b6afcf036705109ec991dd5a03
BrynOvalles <brynovallesburgos@gmail.com>
2024-01-29 12:13:50 -0400

    Creando la Clase Sistema y Tickets

---
+++ Models/Sistema.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prio.Models;
+
+public class Sistema
+{
+    [Key]
+    public int SistemaID { get; set; }
+    [Required(ErrorMessage = "Campo {0} es requerido")]
+    public string? Nombre { get; set; }
+}
---
+++ Models/Tickets.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Prio.Models;
+
+public class Tickets
+{
+    [Key]
+    public int TicketID { get; set; }
+    [Required(ErrorMessage = "Ingresar Fecha.")]
+    [DataType(DataType.Date)]
+    public DateTime Fecha { get; set; } = DateTime.Today;
+    [ForeignKey("Clientes")]
+    [Required(ErrorMessage = " Campo {0} es requerido")]
+    public int ClienteID { get; set; }
+    [ForeignKey("Sistema")]
+    [Required(ErrorMessage = " Campo {0} es requerido")]
+    public int SistemaID { get; set; }
+    [ForeignKey("Prioridades")]
+    [Required(ErrorMessage = " Campo {0} es requerido")]
+    public int PrioridadID { get; set; }
+    [Required(ErrorMessage = " Campo {0} es requerido")]
+    public string? Solicitante { get; set; }
+    [Required(ErrorMessage = " Campo {0} es requerido")]
+    public string? Asunto { get; set; }
+    [Required(ErrorMessage = " Campo {0} es requerido")]
+    public string? Descripcion { get; set; }
+}
